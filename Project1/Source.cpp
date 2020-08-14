56d15d67fad5005a57be2151638be819300178db
saritpat <saritpat@DESKTOP-U2PCN1E>
2020-08-14 14:39:35 +0700

    1d

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,6 +1,22 @@
-#include<stdio.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#define _CRT_SECURE_NO_WARNINGS
+
 int main()
 {
-	printf("123123");
-	return 0;
+    float a, b, d;
+    scanf("%f %f", &a, &b);
+    d = a - b;
+    if (d < 0)
+    {
+        d = d * -1;
+        printf("%.2f", d);
+    }
+    else
+    {
+        printf("%.2f", d);
+    }
+
+    return 0;
 }
